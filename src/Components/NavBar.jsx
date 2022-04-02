a59ccef5990c319067a47bdd24b78786c695c2cb
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-04-02 17:26:38 -0500

    capture value of selected dropdown menu item and save to a stateful variable, update html tags to react-bootstrap tags on NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,71 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import "./SearchBar.jsx";
 
 const NavBar = () => {
+  const [selectValue, setSelectValue] = useState("");
+  const handleSelect = (e) => {
+    setSelectValue(e);
+    console.log(e);
+    return selectValue;
+  };
+
   return (
-    <nav className="navbar navbar-light bg-light">
+    <Navbar className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Chris' Music Collection
-        </a>
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="rgb(66, 66, 223)" className="bi bi-music-player-fill" viewBox="0 0 16 16">
-  <path d="M8 12a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-  <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm1 2h6a1 1 0 0 1 1 1v2.5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zm3 12a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-</svg>
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Artist
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Title
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Album
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Genre
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Release Date
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
+        <Navbar.Brand>Chris' Music Collection</Navbar.Brand>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="rgb(66, 66, 223)"
+          className="bi bi-music-player-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 12a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+          <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm1 2h6a1 1 0 0 1 1 1v2.5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zm3 12a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+        </svg>
+        <DropdownButton
+          id="navbarDropdown"
+          role="button"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item
+            className="dropdown-item"
+            eventKey="artist"
+            onSelect={handleSelect}
+          >
+            Artist
+          </Dropdown.Item>
+          <Dropdown.Item className="dropdown-item" eventKey="title">
+            Title
+          </Dropdown.Item>
+
+          <Dropdown.Item className="dropdown-item" eventKey="album">
+            Album
+          </Dropdown.Item>
+
+          <Dropdown.Item className="dropdown-item" eventKey="genre">
+            Genre
+          </Dropdown.Item>
+
+          <Dropdown.Item className="dropdown-item" eventKey="releaseDate">
+            Release Date
+          </Dropdown.Item>
+        </DropdownButton>
       </div>
-    </nav>
+    </Navbar>
   );
 };
 
