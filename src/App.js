d50da76e6aee3bf2a61867c85bae50ae0b937791
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-04-01 21:58:29 -0500

    add Navbar component and bootstrap-icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <NavBar />
     </div>
   );
 }
